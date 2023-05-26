66c84398958fc8c48324f98ca53611b046660139
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-05-26 12:02:00 +0530

    Add files via upload

--- designweb/urls.py
+++ designweb/urls.py
@@ -5,6 +5,10 @@ urlpatterns=[
     path('about-us',views.about,name='about'),
     path('services',views.services,name='services'),
     path('contacts',views.contacts,name='contacts'),
-    path('gallery',views.gallery,name='gallery')
+    path('gallery',views.gallery,name='gallery'),
+    path('login',views.login,name='login'),
+    path('profile',views.profile,name='profile'),
+    path('editprofile',views.editprofile,name='editprofile'),
+    path('deleteprofile',views.deleteprofile,name='deleteprofile')
 
 ]
--- designweb/views.py
+++ designweb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Registration
+from django.http import HttpResponse
 def index(request):
     if request.method=="POST":
         obj = Registration(username=request.POST["txtuser"],password=request.POST["txtpass"],email=request.POST["txtemail"],mobile=request.POST["txtmobile"])
@@ -7,11 +8,26 @@ def index(request):
         return render(request,"designweb/index.html",{"key":"Inserted Successfully"})
     return render(request,"designweb/index.html")
 def about(request):
-    obj = Registration.objects.all()
-    return render(request,"designweb/about.html",{'res':obj})
+    return render(request,"designweb/about.html")
 def services(request):
     return render(request,"designweb/services.html")
 def gallery(request):
     return render(request,"designweb/gallery.html")
 def contacts(request):
     return render(request,"designweb/contact.html")
+def login(request):
+    if request.method=="POST":
+        res= Registration.objects.filter(username=request.POST["txtuser"],password=request.POST["txtpass"]);
+        if(res.count()>0):
+            return redirect('profile')
+        else:
+            return render(request,"designweb/login.html",{"res":'invalid userid and password'})    
+    return render(request,"designweb/login.html")    
+def profile(request):
+    obj = Registration.objects.all()
+    return render(request,"designweb/dashboard/profile.html",{'res':obj})
+def editprofile(request):
+    return HttpResponse("Edit")
+
+def deleteprofile(request):
+    return HttpResponse("Delete")
